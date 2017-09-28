3c91f67ebfbdd3993b4bb42adbe0d634b1da1f3c
Jacob <jacob5holding@hotmail.com>
2017-09-28 10:40:16 +0100

    pulls webpage from bucket, unzips and leaves in right location

--- userData.sh
+++ userData.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 yum update -y
 yum install -y httpd24 php56 mysql55-server php56-mysqlnd
 service httpd start
 chkconfig httpd on
 groupadd www
-usermod -a -G www ec2-user
+usermod -a -G www vagrant
 chown -R root:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www -type f -exec chmod 0664 {} +
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
-echo ifconfig
+cd /var/www/
+rm -rf html
+wget https://s3.eu-west-2.amazonaws.com/qacfridayexample/html.zip
+unzip html
+mv NBGardens html
